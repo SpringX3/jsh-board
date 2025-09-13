6c006e76c864d4db87cadef86be8157b9bc4d8af
lazyjsh03 <lazy.jsh03@gmail.com>
2025-09-14 03:13:51 +0900

    feat: 테스트 환경을 위한 TestContainers 라이브러리 추가, 설정 코드 추가

--- src/test/java/jsh/board/BoardApplicationTests.java
+++ src/test/java/jsh/board/BoardApplicationTests.java
@@ -2,9 +2,27 @@ package jsh.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BoardApplicationTests {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
+
+    @DynamicPropertySource
+    static void configurationProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+        registry.add("spring.java.hibernate.ddl-auto", () -> "update");
+    }
 
 	@Test
 	void contextLoads() {
