c307cc994e7c1be82929e63aa61acd4902e3cb27
lazyjsh03 <lazy.jsh03@gmail.com>
2025-12-25 08:34:12 +0900

    chore: prometheus, grafana 설정

--- src/main/java/jsh/board/config/SecurityConfig.java
+++ src/main/java/jsh/board/config/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                                 // "/**"는 모든 경로를 의미합니다.
                                 // 모든 경로에 대한 요청을 인증 없이 허용(permitAll)합니다.
-                        .requestMatchers("/", "/favicon.ico").permitAll()
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs.yaml", "/v3/api-docs/**", "/api/health").permitAll()
+                        .requestMatchers("/", "/favicon.ico", "/api/health", "/actuator/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs.yaml", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
                         .anyRequest().authenticated()
                 )
